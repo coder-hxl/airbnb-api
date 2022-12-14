8c3fbbd883b40b07d7915a18b8a850c465146e03
coderhxl <coderhxl@outlook.com>
2022-12-14 21:12:39 +0800

    超过1MB的图片不保存

--- roomData/insertPictureData.ts
+++ roomData/insertPictureData.ts
@@ -10,59 +10,87 @@ import * as roomData from './roomData'
 import type { IData } from './types'
 
 interface ICounter {
-  sum: number
-  current: number
+  name: string
+  expectSum: number
+  saveSum: number
+  discardSum: number
 }
 
 function storeToDatabase(
-  storePath: string,
   filename: string,
   roomId: number,
-  region: string,
+  fileSize: number,
   counter: ICounter
 ) {
   // 储存到数据库
-  fs.stat(storePath, (err, res) => {
-    const { size } = res
-    const url = `${APP_HOST}:${APP_PORT}/api/room_picture/${roomId}/${filename}`
-    const statement = `INSERT INTO room_picture (url, filename, mimetype, size, roomId) VALUES (?, ?, ?, ?, ?);`
+  const url = `${APP_HOST}:${APP_PORT}/api/room_picture/${roomId}/${filename}`
+  const statement = `INSERT INTO room_picture (url, filename, mimetype, size, roomId) VALUES (?, ?, ?, ?, ?);`
 
-    pool
-      .execute(statement, [url, filename, 'image/jpeg', size, roomId])
-      .then(() => {
-        if (counter.sum === ++counter.current) console.log(`${region} 完成~`)
-      })
-  })
+  pool
+    .execute(statement, [url, filename, 'image/jpeg', fileSize, roomId])
+    .then(() => {
+      const { name, expectSum, saveSum, discardSum } = counter
+
+      console.log(
+        `| ${name} | 期待: ${expectSum} | 当前: ${
+          1 + saveSum + discardSum
+        } | 保留: ${1 + saveSum} | 丢弃: ${discardSum}  |`
+      )
+
+      if (counter.expectSum == ++counter.saveSum + counter.discardSum) {
+        console.log(`${name} 完成~`)
+      }
+    })
 }
-
+const agent = new https.Agent({ keepAlive: true })
 function installImg(
   url: string,
   storePath: string,
   filename: string,
   roomId: number,
-  region: string,
   counter: ICounter
 ) {
-  https
-    .get(url, (res) => {
-      const content: Buffer[] = []
-      res.on('data', (d) => content.push(d))
+  const getRes = https.get(url, { agent }, (res) => {
+    const content: Buffer[] = []
+    res.on('data', (d) => content.push(d))
 
-      res.on('end', () => {
-        const buffer = Buffer.concat(content)
-        fs.writeFile(storePath, buffer, () =>
-          storeToDatabase(storePath, filename, roomId, region, counter)
-        )
-      })
+    res.on('end', () => {
+      const buffer = Buffer.concat(content)
+      const size = buffer.byteLength
+
+      // 图片超出 1MB 就不保存
+      if (size > 1024000) {
+        counter.discardSum++
+        return
+      }
+
+      fs.writeFile(storePath, buffer, () =>
+        storeToDatabase(filename, roomId, size, counter)
+      )
     })
-    .on('error', (err) => console.log(err.message))
+  })
+
+  getRes.on('error', async (err: any) => {
+    console.log(getRes.reusedSocket, err.code)
+    if (getRes.reusedSocket && err.code == 'ECONNRESET') {
+      installImg(url, storePath, filename, roomId, counter)
+    } else {
+      counter.discardSum++
+    }
+  })
 }
 
 async function imgHandle(data: IData) {
   const { region, list } = data
-  const counter: ICounter = { sum: 0, current: 0 }
-  let notReq = true
-  console.log(`开始 ${region} 地区的图片下载~`)
+
+  const counter: ICounter = {
+    name: region,
+    expectSum: 0,
+    saveSum: 0,
+    discardSum: 0
+  }
+
+  console.log(`开始下载 ${region} 房间图片~`)
 
   list.forEach((item) => {
     const { id } = item
@@ -70,21 +98,22 @@ async function imgHandle(data: IData) {
 
     if (fs.existsSync(fileDir)) return
 
-    notReq = false
     fs.mkdirSync(fileDir)
 
     item.pictureUrl.forEach((url) => {
-      counter.sum++
+      counter.expectSum++
 
       const time = new Date().getTime()
       const filename = `${time}r${id}.jpg`
       const path = `${fileDir}/${filename}`
 
-      installImg(url, path, filename, id, region, counter)
+      installImg(url, path, filename, id, counter)
     })
   })
 
-  notReq && console.log(`${region} 完成~`)
+  if (counter.expectSum === 0) {
+    console.log(`无需下载 ${region} 房间图片~`)
+  }
 }
 
 const keys = Object.keys(roomData)
