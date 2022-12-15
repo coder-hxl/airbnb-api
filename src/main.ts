3bb1f2e86c496f7a7d439e9899a8926443462af8
coderhxl <coderhxl@outlook.com>
2022-12-15 19:12:48 +0800

    其他

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import '@/app/database'
 
 import { APP_PORT } from '@/app/config'
 
-import 'roomData'
+// 请求 room 数据到数据库中
+// import 'roomData'
 
 app.listen(APP_PORT, () => {
   console.log(`服务器在 ${APP_PORT} 端口启动成功~`)
