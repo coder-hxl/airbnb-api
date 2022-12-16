86a7984a89a29ceafa61225a0c8abc5713dd3f1b
coderhxl <coderhxl@outlook.com>
2022-12-16 19:41:52 +0800

    github图床

--- src/constants/filepath.ts
+++ src/constants/filepath.ts
@@ -1,5 +1,10 @@
 import path from 'path'
 
+export const GITHUB_MAP =
+  'https://raw.githubusercontent.com/coder-hxl/airbnb-upload/master'
+
+export const GITHUB_MAP_ROOM_PICTURE = GITHUB_MAP + '/room'
+
 export const UPLOAD_PATH = path.resolve(__dirname, '../../upload')
 
 export const ROOM_PICTURE_PATH = path.resolve(UPLOAD_PATH, 'room')
