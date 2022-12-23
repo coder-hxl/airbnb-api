f2a0f10f9a3c104b69e5a15f45f67de6f5c67d55
coderhxl <coderhxl@outlook.com>
2022-12-23 21:04:47 +0800

    database

--- database/airbnb-query.sql
+++ database/airbnb-query.sql
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS `room`(
 	user_id INT NOT NULL,
 	create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	
+
 	FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS `room_room_bed_type`(
 	bed_id INT NOT NULL,
 	create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	
+
 	FOREIGN KEY (room_id) REFERENCES room(id) ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY (bed_id) REFERENCES room_bed_type(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -58,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `room_review`(
 	room_id INT NOT NULL,
 	create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	
+
 	FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY (room_id) REFERENCES room(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -72,7 +72,7 @@ CREATE TABLE IF NOT EXISTS `room_picture`(
 	room_id INT NOT NULL,
 	create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	
+
 	FOREIGN KEY (room_id) REFERENCES room(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
@@ -90,89 +90,88 @@ ALTER TABLE user RENAME COLUMN updateAt TO update_at;
 ALTER TABLE room MODIFY introduction VARCHAR(10000);
 
 -- room_bed_type
-INSERT INTO room_bed_type (name) 
+INSERT INTO room_bed_type (name)
 VALUES ('单人房'), ('双床房'), ('三人间'), ('大床房'), ('床位');
 
-
--- r_room_room_type_tab
-
 -- review
-INSERT INTO review (star_rating, comment, user_id, room_id) VALUES 
-	(5, '房间非常好，床睡得舒服， 干净又卫生，设备齐全', 1, 62136475), 
-	(5, '位置非常好，服务很好，周边还有好吃好玩的', 1, 62136475),
-	(5, '环境优美，住得很舒服，位置好', 1, 166746036),
-	(5, '环境优美，住得很舒服，位置好', 2, 62136475);
+INSERT INTO room_review (star_rating, comment, user_id, room_id) VALUES
+	(5, '房间非常好，床睡得舒服， 干净又卫生，设备齐全', 1, 169099602),
+	(5, '位置非常好，服务很好，周边还有好吃好玩的', 1, 169099602),
+	(5, '环境优美，住得很舒服，位置好', 2, 169099602),
+	(5, '环境优美，住得很舒服，位置好', 1, 171868687);
 
--- room_picture
+
+
 
 
 
 
 
 -- query
-SELECT 
-  r.id, r.name, r.introduce, r.create_at createAt, r.update_at updateAt,
- 	JSON_OBJECT('id', u.id, 'name', u.name, 'avatarUrl', u.avatar_url) landlord,
-	JSON_ARRAYAGG(rp.url) pictureUrls,
-  JSON_ARRAYAGG(rt.name) typeTab,
-	JSON_ARRAYAGG(JSON_OBJECT(
-		'id', re.id, 'starRating', re.star_rating, 
-		'comment', re.comment, 'createAt', re.create_at, 
-		'user', JSON_OBJECT('id', reu.id, 'name', reu.name, 'avatarUrl', reu.avatar_url)
-	)) reviews
-FROM room r
-LEFT JOIN user u ON u.id = r.user_id
-LEFT JOIN room_picture rp ON r.id = rp.room_id
-LEFT JOIN r_room_room_type_tab rrt ON r.id = rrt.room_id
-LEFT JOIN room_type_tab rt ON rrt.room_type_tab_id = rt.id
-LEFT JOIN review re ON r.id = re.room_id
-LEFT JOIN user reu ON reu.id = re.user_id
-WHERE r.id = 62136475
-GROUP BY r.id;
 
+-- 通过 POLYGON + POINT 找位置
+SELECT id, deep, name, ext_path FROM area
+WHERE ST_Intersects(polygon, ST_GeomFromText('POINT(111.947966 21.612355)',0))=1;
 
 
 SELECT
-	r.id, r.name, r.introduce, r.address, r.price,
-	JSON_OBJECT('id', u.id, 'name', u.name, 'avatarUel', u.avatar_url) landlord
+	id, name, ext_path, deep,
+	(SELECT
+		JSON_ARRAYAGG(r.id)
+	FROM room r WHERE ST_Intersects(polygon, r.geo)=1) rooms
+FROM area WHERE ext_path LIKE '%阳江市%' AND deep = 2;
+
+
+SELECT
+	id, name, ext_path, deep,
+	(SELECT
+		COUNT(*)
+	FROM room r WHERE ST_Intersects(polygon, r.geo)=1) room_count
+FROM area WHERE ext_path LIKE '%阳江市%' AND deep = 2;
+
+
+-- 查询某个房间
+SELECT
+	r.id, r.name, r.introduction, r.address, r.area_name areaName, r.price,
+	r.type, r.cover_url coverUrl, r.geo,
+	JSON_OBJECT('id', u.id, 'name', u.name, 'avatarUrl', u.avatar_url) landlord,
+	(SELECT ext_path FROM area WHERE ST_Intersects(polygon, r.geo)=1 AND deep = 2) areaExtPath
 FROM room r
-LEFT JOIN user u ON r.user_id = u.id
-WHERE r.region_id = 13;
+LEFT JOIN user u ON u.id = r.user_id
+WHERE r.id = 169099602;
 
-SELECT JSON_ARRAYAGG(url) pictureUrls FROM room_picture WHERE room_id = 62136475;
+SELECT JSON_ARRAYAGG(url) pictureUrls FROM room_picture WHERE room_id = 169099602;
 
-SELECT JSON_ARRAYAGG(rt.name) typeTabs 
-FROM r_room_room_type_tab rrt
-LEFT JOIN room_type_tab rt ON rt.id = rrt.room_type_tab_id
-WHERE rrt.room_id = 62136475;
+SELECT JSON_ARRAYAGG(rbt.name) bedTypes
+FROM room_room_bed_type rrbt
+LEFT JOIN room_bed_type rbt ON rbt.id = rrbt.bed_id
+WHERE rrbt.room_id = 169099602;
 
 SELECT
 	ROUND(AVG(r.star_rating), 1) starRating, COUNT(*) reviewsCount,
 	JSON_ARRAYAGG(
-		JSON_OBJECT('id', r.id, 'star_rating', r.star_rating, 
+		JSON_OBJECT('id', r.id, 'star_rating', r.star_rating,
 			'comment', r.comment, 'createAt', r.create_at,
 			'user', JSON_OBJECT('id', u.id, 'name', u.name, 'avatar_url', u.avatar_url))
 	) reviews
-FROM review r
+FROM room_review r
 LEFT JOIN user u ON r.user_id = u.id
-WHERE room_id = 62136475;
+WHERE room_id = 169099602;
+
+
+-- Home数据
+SELECT
+	id, name, ext_path, deep,
+	(SELECT
+		JSON_ARRAYAGG(JSON_OBJECT('id', r.id, 'name', r.name,
+		'price', r.price, 'type', r.type, 'coverUrl', r.cover_url))
+	FROM room r WHERE ST_Intersects(polygon, r.geo)=1) rooms
+FROM area WHERE ext_path LIKE '%阳江市%' AND deep = 2;
 
 
 
--- POINT(111.947966 21.612355)
--- POINT(111.94866 21.61243)
--- 通过房间经纬度找地区
-SELECT id, deep, name, ext_path FROM area
-WHERE ST_Intersects(polygon, ST_GeomFromText('POINT(111.947966 21.612355)',0))=1;
-
--- 通过地区经纬度找房间
-SELECT * FROM room WHERE ST_Intersects((SELECT polygon FROM area WHERE name='阳江市'), geo)=1;
 
 
-SELECT id, name, ext_path FROM area WHERE ext_path LIKE '%广州%' AND deep = 2;
-
-
-UPDATE room SET cover_url = '1' WHERE id = 6311787;
 
 
 
