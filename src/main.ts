c9fa4676357f77eb4a1a341351d729e2568786fa
coderhxl <coderhxl@outlook.com>
2022-12-31 14:55:42 +0800

    添加新数据

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import '@/app/database'
 import { APP_PORT } from '@/app/config'
 
 // 请求 room 数据到数据库中
-// import 'roomData'
+import 'roomData'
 
 app.listen(APP_PORT, () => {
   console.log(`服务器在 ${APP_PORT} 端口启动成功~`)
