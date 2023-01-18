e6b71b7f2301932b3e5e028f1884f0d56059cf96
coderhxl <coderhxl@outlook.com>
2023-01-18 15:37:25 +0800

    自定义存储：上传的头像会保留后缀，方便使用 github 图床

--- src/middleware/fileMiddleware.ts
+++ src/middleware/fileMiddleware.ts
@@ -2,5 +2,17 @@ import multer from '@koa/multer'
 
 import { AVATAR_PATCH } from '@/constants/filepath'
 
-const avatarUpload = multer({ dest: AVATAR_PATCH })
+const avatarUpload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, AVATAR_PATCH)
+    },
+    filename(req, file, cb) {
+      const unique = Date.now() + '-' + Math.round(Math.random() * 1e9)
+      const suffix = file.originalname.split('.').pop()
+      const filename = `${unique}.${suffix}`
+      cb(null, filename)
+    }
+  })
+})
 export const avatarHandle = avatarUpload.single('avatar')
